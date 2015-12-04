a670380902f1a8ed2745b08d6e73c6c86f57d951
Mihai Georgescu <mgeorgescu@newstore.com>
2015-12-04 12:01:20 +0200

    Add documentation for ignoreBaseURL

--- DVR/Session.swift
+++ DVR/Session.swift
@@ -8,6 +8,10 @@ public class Session: NSURLSession {
     public let cassetteName: String
     public let backingSession: NSURLSession
     public var recordingEnabled = true
+    /**
+     Bool flag for ignoring the baseURL when comparing the request url with the url stored in the cassette.
+     - Note: Defalt value is false, change to true if you need to ignore the base url and compare only the relative paths of the urls not the entire url.
+     */
     public var ignoreBaseURL = false
 
     private let testBundle: NSBundle
@@ -37,7 +41,7 @@ public class Session: NSURLSession {
         addTask(task)
         return task
     }
-
+    
     public override func dataTaskWithRequest(request: NSURLRequest, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) -> NSURLSessionDataTask {
         let task = SessionDataTask(session: self, request: request, completion: completionHandler)
         addTask(task)
